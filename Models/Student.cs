03cf9896d1476b7b4373be3af334f789b936fdf4
Dominika <do_ma1991@hotmail.com>
2020-11-13 15:01:28 +0000

    Creating Models folder and Student entity

---
+++ Models/Student.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace buddy_up.Models
+{
+    public class Student
+    {
+        public int ID { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string Address { get; set; }
+        public Country CountryId { get; set; }
+
+        public Course CourseId { get; set; }
+        public int YearOfStudy { get; set; }
+        public string EmailAddress { get; set; }
+        public DateTime DateOfBirth { get; set; }
+        public string TelephoneNumber { get; set; }
+
+        public ICollection<Club> Clubs { get; set; }
+
+    }
+}
