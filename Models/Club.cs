a28e65d3e27a8e9c174ea0ab9caa2d6635273e8e
Dominika <do_ma1991@hotmail.com>
2020-12-11 11:49:19 +0000

    Adding data annotations to Club class

--- Models/Club.cs
+++ Models/Club.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Threading.Tasks;
 
@@ -8,8 +9,15 @@ namespace buddy_up.Models
     public class Club
     {
         public int ClubID { get; set; }
+
+        [Required]
+        [StringLength(100, ErrorMessage = "Club name cannot be longer than 100 characters.")]
         public string Name { get; set; }
+
+        [Display(Name = "Type")]
         public ClubType ClubTypeId { get; set; }
+
+        [Display(Name = "Members")]
         public ICollection<StudentClubMembership> StudentClubMemberships { get; set; }
     }
 }
