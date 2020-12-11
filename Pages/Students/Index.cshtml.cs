7edff4d1f4558d0a1965e869e0c24026d0c5d7ef
Dominika <do_ma1991@hotmail.com>
2020-12-11 13:10:03 +0000

    Displaying Has Buddy on homepage

--- Pages/Students/Index.cshtml.cs
+++ Pages/Students/Index.cshtml.cs
@@ -20,6 +20,7 @@ namespace buddy_up.Pages.Students
         }
 
         public IList<Student> Student { get;set; }
+        public IList<BuddyMatch> BuddyMatch { get; set; }
 
         public async Task OnGetAsync()
         {
@@ -27,6 +28,12 @@ namespace buddy_up.Pages.Students
                 .Include(s => s.CourseId)
                 .Include(s => s.StudentClubMemberships)
                     .ThenInclude(s => s.Club)
+                .AsNoTracking()
+                .ToListAsync();
+
+            BuddyMatch = await _context.BuddyMatch
+                .Include(bm => bm.MenteeId)
+                .Include(bm => bm.MentorId)
                 .AsNoTracking()
                 .ToListAsync();
         }
