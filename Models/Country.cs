8ef9ae7b67f659b4d5034204aeabc1969c8cbaad
Dominika <do_ma1991@hotmail.com>
2020-12-11 11:53:12 +0000

    Adding data annotations to Country class

--- Models/Country.cs
+++ Models/Country.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Threading.Tasks;
 
@@ -8,6 +9,8 @@ namespace buddy_up.Models
     public class Country
     {
         public int CountryID { get; set; }
+        [Required]
+        [Display(Name= "Country")]
         public string Name { get; set; }
     }
 }
