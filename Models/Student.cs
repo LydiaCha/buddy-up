4236eb3b897ca821867d37fa7af5ba35f1ac4b37
Dominika <do_ma1991@hotmail.com>
2020-12-11 12:22:30 +0000

    Making an email a required field

--- Models/Student.cs
+++ Models/Student.cs
@@ -29,6 +29,7 @@ namespace buddy_up.Models
         [Required]
         [Display(Name = "Year of Study")]
         public int YearOfStudy { get; set; }
+        [Required]
         [Display(Name = "Email Address")]
         [RegularExpression(@"[a-z0-9!#$%&'*+/=?^_`{|}~-]+(?:\.[a-z0-9!#$%&'*+/=?^_`{|}~-]+)*@(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?\.)+[a-z0-9](?:[a-z0-9-]*[a-z0-9])?)", ErrorMessage = "Please enter a valid email address.")]
         public string EmailAddress { get; set; }
