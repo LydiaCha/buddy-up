468ce55344bdd22d4566c333acd9cb9b5e47cea2
Dominika <do_ma1991@hotmail.com>
2021-01-08 17:01:25 +0000

    fixing buddy check bug in index.cshtml

---
+++ Migrations/20210108123241_updating-sample-data.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace buddy_up.Migrations
+{
+    public partial class updatingsampledata : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+
+        }
+    }
+}
